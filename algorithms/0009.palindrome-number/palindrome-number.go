6c82cd760441aba70351bdac316ee765fdfed129
glandogear <glandogear@gmail.com>
2019-07-07 23:00:54 +0800

    leetcode0009 add comment

--- algorithms/0009.palindrome-number/palindrome-number.go
+++ algorithms/0009.palindrome-number/palindrome-number.go
@@ -8,6 +8,10 @@ func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
 	}
+	// I see a solution has exactly same solution with me except he uses []rune instead of string.
+	// His Code cost 0ms and mine cost 20ms.
+	// It's somewhere we chan go deep in to.
+	// s := []rune(strconv.Itoa(x))
 	s := strconv.Itoa(x)
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		if s[i] != s[j] {
